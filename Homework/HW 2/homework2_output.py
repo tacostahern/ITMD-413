09075d9ba96be08cdec02951c5fa2cbf0008ce8c
tacostahern@gmail.com <tacostahern@gmail.com>
2021-01-31 15:41:50 -0600

    Saved output with all test cases

---
+++ Homework/HW 2/homework2_output.py
@@ -0,0 +1,260 @@
+Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 23:11:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\tacos\Documents\ITMD-413\Homework\HW 2\homework2_source_code.py
+Year  1
+USA Population:  326460540
+Mexico Population:  128952560
+Year  2
+USA Population:  324730299
+Mexico Population:  130319457
+Year  3
+USA Population:  323009229
+Mexico Population:  131700843
+Year  4
+USA Population:  321297280
+Mexico Population:  133096872
+Year  5
+USA Population:  319594404
+Mexico Population:  134507699
+Year  6
+USA Population:  317900554
+Mexico Population:  135933481
+Year  7
+USA Population:  316215681
+Mexico Population:  137374376
+Year  8
+USA Population:  314539738
+Mexico Population:  138830544
+Year  9
+USA Population:  312872677
+Mexico Population:  140302148
+Year  10
+USA Population:  311214452
+Mexico Population:  141789351
+Year  11
+USA Population:  309565015
+Mexico Population:  143292318
+Year  12
+USA Population:  307924321
+Mexico Population:  144811216
+Year  13
+USA Population:  306292322
+Mexico Population:  146346215
+Year  14
+USA Population:  304668972
+Mexico Population:  147897485
+Year  15
+USA Population:  303054227
+Mexico Population:  149465198
+Year  16
+USA Population:  301448040
+Mexico Population:  151049529
+Year  17
+USA Population:  299850365
+Mexico Population:  152650655
+Year  18
+USA Population:  298261158
+Mexico Population:  154268751
+Year  19
+USA Population:  296680374
+Mexico Population:  155904000
+Year  20
+USA Population:  295107968
+Mexico Population:  157556583
+Year  21
+USA Population:  293543896
+Mexico Population:  159226682
+Year  22
+USA Population:  291988113
+Mexico Population:  160914485
+Year  23
+USA Population:  290440576
+Mexico Population:  162620179
+Year  24
+USA Population:  288901241
+Mexico Population:  164343953
+Year  25
+USA Population:  287370064
+Mexico Population:  166085999
+Year  26
+USA Population:  285847003
+Mexico Population:  167846510
+Year  27
+USA Population:  284332014
+Mexico Population:  169625683
+Year  28
+USA Population:  282825054
+Mexico Population:  171423715
+Year  29
+USA Population:  281326081
+Mexico Population:  173240807
+Year  30
+USA Population:  279835053
+Mexico Population:  175077159
+Year  31
+USA Population:  278351927
+Mexico Population:  176932977
+Year  32
+USA Population:  276876662
+Mexico Population:  178808467
+Year  33
+USA Population:  275409216
+Mexico Population:  180703837
+Year  34
+USA Population:  273949547
+Mexico Population:  182619297
+Year  35
+USA Population:  272497614
+Mexico Population:  184555062
+Year  36
+USA Population:  271053377
+Mexico Population:  186511345
+Year  37
+USA Population:  269616794
+Mexico Population:  188488366
+Year  38
+USA Population:  268187825
+Mexico Population:  190486342
+Year  39
+USA Population:  266766430
+Mexico Population:  192505498
+Year  40
+USA Population:  265352568
+Mexico Population:  194546056
+Year  41
+USA Population:  263946199
+Mexico Population:  196608244
+Year  42
+USA Population:  262547284
+Mexico Population:  198692291
+Year  43
+USA Population:  261155784
+Mexico Population:  200798430
+Year  44
+USA Population:  259771658
+Mexico Population:  202926893
+Year  45
+USA Population:  258394868
+Mexico Population:  205077918
+Year  46
+USA Population:  257025375
+Mexico Population:  207251744
+Year  47
+USA Population:  255663141
+Mexico Population:  209448613
+Year  48
+USA Population:  254308126
+Mexico Population:  211668768
+Year  49
+USA Population:  252960293
+Mexico Population:  213912457
+Year  50
+USA Population:  251619604
+Mexico Population:  216179929
+Year  51
+USA Population:  250286020
+Mexico Population:  218471436
+Year  52
+USA Population:  248959504
+Mexico Population:  220787233
+Year  53
+USA Population:  247640018
+Mexico Population:  223127578
+Year  54
+USA Population:  246327526
+Mexico Population:  225492730
+Year  55
+USA Population:  245021990
+Mexico Population:  227882953
+Year  56
+USA Population:  243723374
+Mexico Population:  230298513
+Year  57
+USA Population:  242431640
+Mexico Population:  232739677
+Year  58
+USA Population:  241146752
+Mexico Population:  235206717
+Year  59
+USA Population:  239868675
+Mexico Population:  237699909
+Year  60
+USA Population:  238597371
+Mexico Population:  240219528
+Number of years it took for Mexico's population to overtake USA: 60 years
+What color are the status lights? Enter Green, Red, or Amber: Red
+Shut off all input lines and check meter #3
+What is the value in meter #3? 
+= RESTART: C:\Users\tacos\Documents\ITMD-413\Homework\HW 2\homework2_source_code.py
+What color are the status lights? Enter Green, Red, or Amber: Green
+Do restart procedure
+>>> 
+= RESTART: C:\Users\tacos\Documents\ITMD-413\Homework\HW 2\homework2_source_code.py
+What color are the status lights? Enter Green, Red, or Amber: Greeen
+That is not a valid light status color!
+>>> 
+= RESTART: C:\Users\tacos\Documents\ITMD-413\Homework\HW 2\homework2_source_code.py
+What color are the status lights? Enter Green, Red, or Amber: Red
+Shut off all input lines and check meter #3
+What is the value in meter #3? 51
+Measure flow velocity at inlet 2B
+What is the flow velocity? Enter High, Normal, or Low: High
+Refer unit for factory service
+>>> 
+= RESTART: C:\Users\tacos\Documents\ITMD-413\Homework\HW 2\homework2_source_code.py
+What color are the status lights? Enter Green, Red, or Amber: Red
+Shut off all input lines and check meter #3
+What is the value in meter #3? 51
+Measure flow velocity at inlet 2B
+What is the flow velocity? Enter High, Normal, or Low: Low
+Refer unit for factory service
+>>> 
+= RESTART: C:\Users\tacos\Documents\ITMD-413\Homework\HW 2\homework2_source_code.py
+What color are the status lights? Enter Green, Red, or Amber: Red
+Shut off all input lines and check meter #3
+What is the value in meter #3? 51
+Measure flow velocity at inlet 2B
+What is the flow velocity? Enter High, Normal, or Low: Normal
+Refer to inlet service manual
+>>> 
+= RESTART: C:\Users\tacos\Documents\ITMD-413\Homework\HW 2\homework2_source_code.py
+What color are the status lights? Enter Green, Red, or Amber: Red
+Shut off all input lines and check meter #3
+What is the value in meter #3? 51
+Measure flow velocity at inlet 2B
+What is the flow velocity? Enter High, Normal, or Low: normal
+That is not a valid flow velocity!
+>>> 
+= RESTART: C:\Users\tacos\Documents\ITMD-413\Homework\HW 2\homework2_source_code.py
+What color are the status lights? Enter Green, Red, or Amber: Red
+Shut off all input lines and check meter #3
+What is the value in meter #3? 49
+Check main line for test pressure
+What is the test pressure? Enter High, Normal, or Low: High
+Refer to main line manual
+>>> 
+= RESTART: C:\Users\tacos\Documents\ITMD-413\Homework\HW 2\homework2_source_code.py
+What color are the status lights? Enter Green, Red, or Amber: Red
+Shut off all input lines and check meter #3
+What is the value in meter #3? 49
+Check main line for test pressure
+What is the test pressure? Enter High, Normal, or Low: Low
+Refer to main line manual
+>>> 
+= RESTART: C:\Users\tacos\Documents\ITMD-413\Homework\HW 2\homework2_source_code.py
+What color are the status lights? Enter Green, Red, or Amber: Red
+Shut off all input lines and check meter #3
+What is the value in meter #3? 49
+Check main line for test pressure
+What is the test pressure? Enter High, Normal, or Low: Normal
+Refer to motor service manual
+>>> 
+= RESTART: C:\Users\tacos\Documents\ITMD-413\Homework\HW 2\homework2_source_code.py
+What color are the status lights? Enter Green, Red, or Amber: Red
+Shut off all input lines and check meter #3
+What is the value in meter #3? 49
+Check main line for test pressure
+What is the test pressure? Enter High, Normal, or Low: ow
+That is not a valid test pressure!
+>>> 

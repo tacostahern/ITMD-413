c4878058de8cf26eb1464a7d47e70557c3bf35bd
tacostahern@gmail.com <tacostahern@gmail.com>
2021-02-14 16:22:16 -0600

    sample output

---
+++ Homework/HW 4/SellIItems_output.py
@@ -0,0 +1,69 @@
+Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 23:11:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\tacos\OneDrive - hawk.iit.edu\Documents\ITMD-413\Homework\HW 4\SellItems.py
+
+Please enter an item price, or enter '0' to stop: 100
+Please enter an item price, or enter '0' to stop: 100
+Please enter an item price, or enter '0' to stop: 100
+Please enter an item price, or enter '0' to stop: 0
+Item Prices: [100, 100, 100]
+Total price: $300.00
+Taxes: $21.00
+Final Cost: $321.00
+Please enter a value in the matrix: 1
+Please enter a value in the matrix: 2
+Please enter a value in the matrix: 3
+Please enter a value in the matrix: 4
+Please enter a value in the matrix: 5
+Please enter a value in the matrix: 6
+Please enter a value in the matrix: 7
+Please enter a value in the matrix: 8
+Please enter a value in the matrix: 9
+Please enter a value in the other matrix: 1
+Please enter a value in the other matrix: 2
+Please enter a value in the other matrix: 3
+Please enter a value in the other matrix: 4
+Please enter a value in the other matrix: 5
+Please enter a value in the other matrix: 6
+Please enter a value in the other matrix: 7
+Please enter a value in the other matrix: 8
+Please enter a value in the other matrix: 9
+Matrix A: [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+Matrix B: [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+Their product: [30, 36, 42, 66, 81, 96, 102, 126, 150]
+>>> 
+= RESTART: C:\Users\tacos\OneDrive - hawk.iit.edu\Documents\ITMD-413\Homework\HW 4\SellItems.py
+
+Please enter an item price, or enter '0' to stop: 1
+Please enter an item price, or enter '0' to stop: 2
+Please enter an item price, or enter '0' to stop: -3
+ERROR: A price cannot be negative.
+Please enter an item price, or enter '0' to stop: 3
+Please enter an item price, or enter '0' to stop: 0
+Item Prices: [1, 2, 3]
+Total price: $6.00
+Taxes: $0.42
+Final Cost: $6.42
+Please enter a value in the matrix: 1
+Please enter a value in the matrix: 2
+Please enter a value in the matrix: 3
+Please enter a value in the matrix: 4
+Please enter a value in the matrix: 5
+Please enter a value in the matrix: 6
+Please enter a value in the matrix: 7
+Please enter a value in the matrix: 8
+Please enter a value in the matrix: 9
+Please enter a value in the other matrix: 0
+Please enter a value in the other matrix: 2.0
+Please enter a value in the other matrix: 4.0
+Please enter a value in the other matrix: 1
+Please enter a value in the other matrix: 4.5
+Please enter a value in the other matrix: 2.2
+Please enter a value in the other matrix: 1.1
+Please enter a value in the other matrix: 4.3
+Please enter a value in the other matrix: 5.2
+Matrix A: [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+Matrix B: [[0, 2.0, 4.0], [1, 4.5, 2.2], [1.1, 4.3, 5.2]]
+Their product: [5.300000000000001, 23.9, 24.0, 11.600000000000001, 56.3, 58.2, 17.9, 88.69999999999999, 92.4]
+>>> 

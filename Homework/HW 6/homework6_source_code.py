040916a24c3ca86e19669cf7987a13ea6a6ebe33
tacostahern@gmail.com <tacostahern@gmail.com>
2021-02-28 11:47:36 -0600

    initial creation of homework 6 file, and the research paper

---
+++ Homework/HW 6/homework6_source_code.py
@@ -0,0 +1,9 @@
+'''
+Program Author: Tony Acosta Hernandez
+Course: ITMD 413
+Program Description: This program will test the efficiency of multiple sorting
+algorithms across lists of multiple sizes which include 10000, 30000, 50000,
+70000, and 90000 size arrays. The sorting algorithms that will be tested include
+bubble sort, shell sort, and quick sort. The results will be recorded and
+plotted in a graph to show the different times.
+'''

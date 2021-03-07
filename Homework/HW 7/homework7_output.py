033ffc06f29f3894b043424951e021f430c22787
tacostahern@gmail.com <tacostahern@gmail.com>
2021-03-07 17:58:30 -0600

    added output for hw 7

---
+++ Homework/HW 7/homework7_output.py
@@ -0,0 +1,42 @@
+Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 23:11:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\tacos\OneDrive - hawk.iit.edu\Documents\ITMD-413\Homework\HW 7\homework7_source_code.py
+dn: tacostahern, dc=gmail, dc=com
+cn: Tony Acosta Hernandez
+sn: Acosta Hernandez
+telephoneNumber: (312)852-5498
+
+dn: djablonski, dc=gmail, dc=com
+cn: Daniel Jablonski
+sn: Jablonski
+telephoneNumber: (123)456-7890
+
+dn: amirizarry, dc=gmail, dc=com
+cn: Anthony Irizarry
+sn: Irizarry
+telephoneNumber: (123)456-7899
+
+dn: marroyo, dc=gmail, dc=com
+cn: Madelyn Arroyo
+sn: Arroyo
+telephoneNumber: (123)456-7889
+
+dn: kbarrera, dc=gmail, dc=com
+cn: Kevin Barrera
+sn: Barrera
+telephoneNumber: (123)456-7789
+Please enter 'Y' to enter a phone number, or enter anything else to stop: y
+Please enter the phone number: 3128525498
+The formatted number is (312) 852 5498
+Please enter 'Y' to enter a phone number, or enter anything else to stop: y
+Please enter the phone number: (312)8525498
+The formatted number is (312) 852 5498
+Please enter 'Y' to enter a phone number, or enter anything else to stop: y
+Please enter the phone number: 312 852 5498
+The formatted number is (312) 852 5498
+Please enter 'Y' to enter a phone number, or enter anything else to stop: y
+Please enter the phone number: 312-852-5498
+The formatted number is (312) 852 5498
+Please enter 'Y' to enter a phone number, or enter anything else to stop: n
+>>> 

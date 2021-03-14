c83e66a1dd222b08eba487d3734623b04650dfc5
tacostahern@gmail.com <tacostahern@gmail.com>
2021-03-14 15:56:50 -0500

    saved output for homework

---
+++ Homework/HW 8/homework8_output.py
@@ -0,0 +1,44 @@
+Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 23:11:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\tacos\OneDrive - hawk.iit.edu\Documents\ITMD-413\Homework\HW 8\homework8_source_code.py
+[[ 1  2  3  4  5]
+ [ 6  7  8  9 10]
+ [11 12 13 14 15]]
+Row 2 of array: [[ 6  7  8  9 10]]
+Column 4 of the array: [ 4  9 14]
+Rows 0 and 1 of the array:
+ [[ 1  2  3  4  5]
+ [ 6  7  8  9 10]]
+Columns 2 - 4 of the array:
+ [[ 3  4  5]
+ [ 8  9 10]
+ [13 14 15]]
+Value in row 1 and column 4 of the array: 10
+All elements from rows 1 and 2 that are in columns 0, 2, and 4:
+ [[ 6  8 10]
+ [11 13 15]]
+[[  15.        50.        50.      ...  207.      1442.        94.1868 ]
+ [  15.        50.        50.      ...  197.      1322.        86.34879]
+ [  15.        50.        50.      ...  210.      1510.        98.62834]
+ ...
+ [  15.        50.        50.      ...  200.      1374.        89.74526]
+ [  15.        50.        50.      ...  195.      1287.        84.0627 ]
+ [  15.        50.        50.      ...  210.      1490.        97.32201]]
+Number of students in dataset: 33
+Number of rows in dataset: 33
+Number of columns in dataset: 32
+The array data types are: float64
+Minimum score: 29.65382
+Maximum score: 99.86936
+Mean value: 87.66996787878789
+Median: 92.88047
+Mode: ModeResult(mode=array([94.1868]), count=array([2]))
+Standard Deviation: 15.553686425410683
+25% percentile: 87.1979
+75% percentile: 96.73416
+Number of students with an A: 21
+Number of students with a B: 8
+Number of students with a C: 1
+Number of students with a D: 1
+Number of students with an F: 2

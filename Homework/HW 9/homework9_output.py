47da00aaf1fe9ac8f6fb59a69cb9c2336412bf24
tacostahern@gmail.com <tacostahern@gmail.com>
2021-03-21 22:59:13 -0500

    finished assignment with all parts

---
+++ Homework/HW 9/homework9_output.py
@@ -0,0 +1,159 @@
+Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 23:11:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\tacos\OneDrive - hawk.iit.edu\Documents\ITMD-413\Homework\HW 9\homework9_source_code.py
+count    20.000000
+mean     49.400000
+std      30.401524
+min       3.000000
+25%      21.500000
+50%      46.500000
+75%      75.500000
+max      99.000000
+dtype: float64
+Morning      98.9
+Afternoon    96.8
+Evening      98.6
+Name: Maxine, dtype: float64
+         Maxine  James  Amanda
+Morning    98.9   68.7   100.3
+         Maxine  James  Amanda
+Morning    98.9   68.7   100.3
+Evening    98.6   86.3    93.9
+           Amanda  Maxine
+Morning     100.3    98.9
+Afternoon   103.5    96.8
+Evening      93.9    98.6
+           Amanda  Maxine
+Morning     100.3    98.9
+Afternoon   103.5    96.8
+          Maxine      James      Amanda
+count   3.000000   3.000000    3.000000
+mean   98.100000  84.700000   99.233333
+std     1.135782  15.263027    4.888081
+min    96.800000  68.700000   93.900000
+25%    97.700000  77.500000   97.100000
+50%    98.600000  86.300000  100.300000
+75%    98.750000  92.700000  101.900000
+max    98.900000  99.100000  103.500000
+        Morning  Afternoon  Evening
+Maxine     98.9       96.8     98.6
+James      68.7       99.1     86.3
+Amanda    100.3      103.5     93.9
+           Amanda  James  Maxine
+Morning     100.3   68.7    98.9
+Afternoon   103.5   99.1    96.8
+Evening      93.9   86.3    98.6
+Number of passengers: 1309
+Number of male passengers: 843
+Number of female passengers: 466
+Average age among passengers: age    29.881978
+dtype: float64
+Number of passengers under 21: 249
+Number of passengers that survived: 500
+Number of passengers that perished: 809
+Number of men that survived: 161
+Number of women that survived: 339
+Number of men that perished: 682
+Number of women that perished: 127
+Name of the youngest survivor: 763    Dean, Miss. Elizabeth Gladys M
+Name: Name, dtype: object
+Age of the youngest survivor: age    0.1667
+dtype: float64
+Name of the oldest survivor: 14    Barkworth, Mr. Algernon Henry W
+Name: Name, dtype: object
+Age of the oldest survivor: age    80.0
+dtype: float64
+Names of the survivors:                                  Name
+1      Allison, Master. Hudson Trevor
+5                 Anderson, Mr. Harry
+6     Andrews, Miss. Kornelia Theodos
+8     Appleton, Mrs. Edward Dale (Cha
+11    Astor, Mrs. John Jacob (Madelei
+...                               ...
+1261           Turkula, Mrs. (Hedwig)
+1277             Vartanian, Mr. David
+1286  Whabee, Mrs. George Joseph (Sha
+1290  Wilkes, Mrs. James (Ellen Needs
+1300  Yasbeck, Mrs. Antoni (Selini Al
+
+[499 rows x 1 columns]
+>>> 
+= RESTART: C:\Users\tacos\OneDrive - hawk.iit.edu\Documents\ITMD-413\Homework\HW 9\homework9_source_code.py
+count    20.000000
+mean     42.050000
+std      29.941126
+min       0.000000
+25%      22.250000
+50%      41.500000
+75%      69.250000
+max      87.000000
+dtype: float64
+Morning      98.9
+Afternoon    96.8
+Evening      98.6
+Name: Maxine, dtype: float64
+         Maxine  James  Amanda
+Morning    98.9   68.7   100.3
+         Maxine  James  Amanda
+Morning    98.9   68.7   100.3
+Evening    98.6   86.3    93.9
+           Amanda  Maxine
+Morning     100.3    98.9
+Afternoon   103.5    96.8
+Evening      93.9    98.6
+           Amanda  Maxine
+Morning     100.3    98.9
+Afternoon   103.5    96.8
+          Maxine      James      Amanda
+count   3.000000   3.000000    3.000000
+mean   98.100000  84.700000   99.233333
+std     1.135782  15.263027    4.888081
+min    96.800000  68.700000   93.900000
+25%    97.700000  77.500000   97.100000
+50%    98.600000  86.300000  100.300000
+75%    98.750000  92.700000  101.900000
+max    98.900000  99.100000  103.500000
+        Morning  Afternoon  Evening
+Maxine     98.9       96.8     98.6
+James      68.7       99.1     86.3
+Amanda    100.3      103.5     93.9
+           Amanda  James  Maxine
+Morning     100.3   68.7    98.9
+Afternoon   103.5   99.1    96.8
+Evening      93.9   86.3    98.6
+Number of passengers: 1309
+Number of male passengers: 843
+Number of female passengers: 466
+Average age among passengers: age    29.881978
+dtype: float64
+Number of passengers under 21: 249
+Number of passengers that survived: 500
+Number of passengers that perished: 809
+Number of men that survived: 161
+Number of women that survived: 339
+Number of men that perished: 682
+Number of women that perished: 127
+Name of the youngest survivor: 763    Dean, Miss. Elizabeth Gladys M
+Name: Name, dtype: object
+Age of the youngest survivor: age    0.1667
+dtype: float64
+Name of the oldest survivor: 14    Barkworth, Mr. Algernon Henry W
+Name: Name, dtype: object
+Age of the oldest survivor: age    80.0
+dtype: float64
+Names of the survivors:                                  Name
+1      Allison, Master. Hudson Trevor
+5                 Anderson, Mr. Harry
+6     Andrews, Miss. Kornelia Theodos
+8     Appleton, Mrs. Edward Dale (Cha
+11    Astor, Mrs. John Jacob (Madelei
+...                               ...
+1261           Turkula, Mrs. (Hedwig)
+1277             Vartanian, Mr. David
+1286  Whabee, Mrs. George Joseph (Sha
+1290  Wilkes, Mrs. James (Ellen Needs
+1300  Yasbeck, Mrs. Antoni (Selini Al
+
+[499 rows x 1 columns]
+>>> 

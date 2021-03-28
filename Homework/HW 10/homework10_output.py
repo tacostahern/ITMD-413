b804db2fb0f50a192db29734ce9b683439ad84ee
tacostahern@gmail.com <tacostahern@gmail.com>
2021-03-28 17:35:04 -0500

    created output file

---
+++ Homework/HW 10/homework10_output.py
@@ -0,0 +1,154 @@
+Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 23:11:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\tacos\OneDrive - hawk.iit.edu\Documents\ITMD-413\Homework\HW 10\homework10_source_code.py
+Not empty file
+
+    Welcome. Enter the number corresponding to what you'd like to do.
+
+    1. Add a new name and email address.
+    2. Find an existing email address from a name.
+    3. Change an existing email address.
+    4. Delete an existing name and email address.
+    5. Exit the program
+    
+Enter a number: 1
+Enter the name you would like to add: Tony
+Enter the email you would like to add: tacostahernandez@hawk.iit.edu
+User added
+
+    Welcome. Enter the number corresponding to what you'd like to do.
+
+    1. Add a new name and email address.
+    2. Find an existing email address from a name.
+    3. Change an existing email address.
+    4. Delete an existing name and email address.
+    5. Exit the program
+    
+Enter a number: 1
+Enter the name you would like to add: Anthony
+Enter the email you would like to add: aangeles@hawk.iit.edu
+User added
+
+    Welcome. Enter the number corresponding to what you'd like to do.
+
+    1. Add a new name and email address.
+    2. Find an existing email address from a name.
+    3. Change an existing email address.
+    4. Delete an existing name and email address.
+    5. Exit the program
+    
+Enter a number: 5
+>>> 
+= RESTART: C:\Users\tacos\OneDrive - hawk.iit.edu\Documents\ITMD-413\Homework\HW 10\homework10_source_code.py
+Not empty file
+
+    Welcome. Enter the number corresponding to what you'd like to do.
+
+    1. Add a new name and email address.
+    2. Find an existing email address from a name.
+    3. Change an existing email address.
+    4. Delete an existing name and email address.
+    5. Exit the program
+    
+Enter a number: 2
+Enter the name you would like to search: Tony
+The email is: tacostahernandez@hawk.iit.edu
+
+    Welcome. Enter the number corresponding to what you'd like to do.
+
+    1. Add a new name and email address.
+    2. Find an existing email address from a name.
+    3. Change an existing email address.
+    4. Delete an existing name and email address.
+    5. Exit the program
+    
+Enter a number: 2
+Enter the name you would like to search: Anthony
+The email is: aangeles@hawk.iit.edu
+
+    Welcome. Enter the number corresponding to what you'd like to do.
+
+    1. Add a new name and email address.
+    2. Find an existing email address from a name.
+    3. Change an existing email address.
+    4. Delete an existing name and email address.
+    5. Exit the program
+    
+Enter a number: 4
+Enter the name of the user you want to delete: Anthony
+User deleted
+
+    Welcome. Enter the number corresponding to what you'd like to do.
+
+    1. Add a new name and email address.
+    2. Find an existing email address from a name.
+    3. Change an existing email address.
+    4. Delete an existing name and email address.
+    5. Exit the program
+    
+Enter a number: 3
+Enter the name of the email you would like to change: Tony
+Enter the new email you would like to use: tacostahern@gmail.com
+User email updated
+
+    Welcome. Enter the number corresponding to what you'd like to do.
+
+    1. Add a new name and email address.
+    2. Find an existing email address from a name.
+    3. Change an existing email address.
+    4. Delete an existing name and email address.
+    5. Exit the program
+    
+Enter a number: 1
+Enter the name you would like to add: Anthony Angeles
+Enter the email you would like to add: aangeles@hawk.iit.edu
+User added
+
+    Welcome. Enter the number corresponding to what you'd like to do.
+
+    1. Add a new name and email address.
+    2. Find an existing email address from a name.
+    3. Change an existing email address.
+    4. Delete an existing name and email address.
+    5. Exit the program
+    
+Enter a number: 5
+>>> 
+= RESTART: C:\Users\tacos\OneDrive - hawk.iit.edu\Documents\ITMD-413\Homework\HW 10\homework10_source_code.py
+Not empty file
+
+    Welcome. Enter the number corresponding to what you'd like to do.
+
+    1. Add a new name and email address.
+    2. Find an existing email address from a name.
+    3. Change an existing email address.
+    4. Delete an existing name and email address.
+    5. Exit the program
+    
+Enter a number: 2
+Enter the name you would like to search: Anthony Angeles
+The email is: aangeles@hawk.iit.edu
+
+    Welcome. Enter the number corresponding to what you'd like to do.
+
+    1. Add a new name and email address.
+    2. Find an existing email address from a name.
+    3. Change an existing email address.
+    4. Delete an existing name and email address.
+    5. Exit the program
+    
+Enter a number: 2
+Enter the name you would like to search: Tony
+The email is: tacostahern@gmail.com
+
+    Welcome. Enter the number corresponding to what you'd like to do.
+
+    1. Add a new name and email address.
+    2. Find an existing email address from a name.
+    3. Change an existing email address.
+    4. Delete an existing name and email address.
+    5. Exit the program
+    
+Enter a number: 5
+>>> 

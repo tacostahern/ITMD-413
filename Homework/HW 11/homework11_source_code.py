a1ab77a27c568b38159af7ad774f406723f26ddd
tacostahern@gmail.com <tacostahern@gmail.com>
2021-04-03 17:16:36 -0500

    Initial creation of homework 11 and added the csv files

---
+++ Homework/HW 11/homework11_source_code.py
@@ -0,0 +1,24 @@
+'''
+Program Author: Tony Acosta Hernandez
+Course: ITMD 413
+Program Description: This program will utilize the seaborn library, matplotlib, and several
+datasets to create charts that are meant to visualize the data. It will solve the following
+problems: 
+    a) Using dataset workerstips.csv, create a scatterplot with total_bill as the x-axis and the tips
+    as the y-axis.
+    b) Using dataset workerstips.csv, create a scatter plot that will differentiate between smokers 
+    and non-smokers. Use a different color (hue) and style marker (style) to visually split the 
+    points of scatterplot. Because the default range sizes are limited, increase the range size. 
+    Try the tuple (10, 300) as the size range parameter.
+    c) A simple bar plot will work well to show average tips per day of the week. Find out 
+    which day has the highest average tip. In this case, you may want to show the average tip 
+    (y-axis) per day of the week (x-axis). 
+    d) Repeat part (b) above but this time split the distribution by lunch and dinner. What 
+    information can you derive from the new visualization? 
+    e) Using dataset flightData.csv, create a lineplot to plot average number of passengers per 
+    year per month. Use the year on the x-axis and the passengers on the y-axis. Also, find 
+    out how to plot the total number of passengers per year. 
+    f) Using dataset titanic.csv, show the counts of observations in each categorical bin using 
+    bars plot.
+'''
+

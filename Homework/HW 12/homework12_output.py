18d85e9f54608496c09e585dd6468796a4f86325
tacostahern@gmail.com <tacostahern@gmail.com>
2021-04-11 16:02:06 -0500

    added output file

---
+++ Homework/HW 12/homework12_output.py
@@ -0,0 +1,52 @@
+Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 23:11:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\tacos\OneDrive - hawk.iit.edu\Documents\ITMD-413\Homework\HW 12\shopping.py
+El Cid statistics:
+Address: 2645 N. Kedzie
+Store open? True
+Sales tax percentage:  10.0 %
+Number of people served: 20
+Max occupancy: 100
+Current occupancy: 10
+Price per person: $ 15
+Total sales tax: $ 30.0
+Total sales: $ 330.0
+
+Whole Foods statistics:
+Address: 959 W Fullerton Ave
+Store open? False
+Sales tax percentage:  5.0 %
+Total revenue: $ 10000
+Grocery store type: Grocery
+Total sales tax: $ 500.0
+Total sales: $ 10500.0
+
+El Cid new patrons: 20
+Welcome to El Cid
+New current occupancy: 30
+
+El Cid new patrons: 100
+We are at a capacity, we appreciate your patience
+New current occupancy: 30
+People Served: 40
+People checked out: 20
+Current Occupancy: 20
+
+El Cid updated stats:
+Number of people served: 40
+Max occupancy: 100
+Current occupancy: 20
+Price per person: $ 15
+Total sales tax: $ 60.0
+Total sales: $ 660.0
+
+20 bananas sold at a dollar each
+Whole foods total revenue: $ 10020
+100 cartons of milk sold at 3 dollars each
+Whole foods total revenue: $ 10320
+
+Whole Foods updated stats:
+Total sales tax: $ 516.0
+Total sales: $ 10836.0
+>>> 

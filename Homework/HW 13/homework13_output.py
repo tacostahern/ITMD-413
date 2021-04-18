c9e338e9f00d00dd94a428d9c2decb0dc9dcbb95
tacostahern@gmail.com <tacostahern@gmail.com>
2021-04-18 16:57:49 -0500

    added output files

---
+++ Homework/HW 13/homework13_output.py
@@ -0,0 +1,14 @@
+Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 23:11:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\tacos\OneDrive - hawk.iit.edu\Documents\ITMD-413\Homework\HW 13\password_checker.py
+True
+False
+True
+True
+True
+False
+True
+True
+True
+>>> 
